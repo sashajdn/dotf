489ba0add7c4c16c7833c9976efa86603e398204
sasha <alexperkins.dev@gmail.com>
2022-10-02 19:11:28 +0100

    add(nvim): keymappings

---
+++ nvim-lua/after/plugin/keymap/init.lua
@@ -0,0 +1,6 @@
+local Remap = require("sasha.keymap")
+local nnoremap = Remap.nnoremap
+local vnoremap = Remap.vnoremap
+local inoremap = Remap.inoremap
+local xnoremap = Remap.xnoremap
+local nmap = Remap.nmap
