7893f2969479968ff663ace7177a518ef2266158
sasha <alexperkins.dev@gmail.com>
2024-06-24 22:50:37 +0800

    wip: telescope search dotfiles & wiki

--- nvim-lazy/lua/sasha/plugins/telescope.lua
+++ nvim-lazy/lua/sasha/plugins/telescope.lua
@@ -10,6 +10,24 @@ return {
     local telescope = require("telescope")
     local actions = require("telescope.actions")
 
+    local builtin = require("telescope.builtin")
+
+    local function search_dotfiles()
+      builtin.git_files({
+        prompt_title = "< DotF 💻 >",
+        cwd = "$HOME/dotf",
+        hidden = true,
+      })
+    end
+
+    local function search_wiki()
+      builtin.git_files({
+        prompt_title = "< Wiki 📚>",
+        cwd = "$WIKI",
+        hidden = true,
+      })
+    end
+
     telescope.setup({
       defaults = {
         file_sorter = require("telescope.sorters").get_fzy_sorter,
@@ -36,12 +54,14 @@ return {
 
     telescope.load_extension("fzf")
 
-    local keymap = vim.keymap 
+    local keymap = vim.keymap
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd"} )
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files"} )
-    keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy find string in cwd"} )
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Fuzzy find string under cursor in cwd"} )
+    keymap.set("n", "<leader>ff", "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy find string in cwd" })
+    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Fuzzy find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find TODOs" })
+    keymap.set("n", "<leader>fd", search_dotfiles, { desc = "Search dotf" })
+    keymap.set("n", "<leader>fw", search_wiki, { desc = "Search Wiki" })
   end,
 }
